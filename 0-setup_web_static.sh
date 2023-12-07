6d6dce3e9b57a647133b02fa75f8b600f72d3eae
imad-el-hajjami <contact@imadelhajjami.com>
2023-12-07 09:35:51 -0800

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
-
 apt-get update
 apt-get install -y nginx
-
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
@@ -34,6 +32,4 @@ printf %s "server {
       internal;
     }
 }" > /etc/nginx/sites-available/default
-
 service nginx restart
-
