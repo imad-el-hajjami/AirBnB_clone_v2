ebf3026cf98f1e24cf19329e1a7ca9c065aa0483
imad-el-hajjami <contact@imadelhajjami.com>
2023-12-07 09:40:25 -0800

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,62 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-apt-get update
-apt-get install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# script to install and setup nginx
+CONFIG_FILE="/etc/nginx/sites-available/default"
+HOST_NAME=$(hostname)
+MY_ID=496
+STATIC=/data/web_static
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# check if hostname is correct
+if [[ $(hostname) =~ ^$MY_ID-web-[0-9]+ ]]; then
+    echo 'hostname properly configured'
+else
+    (>&2 echo 'hostname not configured properly...')
+    (>&2 echo 'please set hostname to pattern: 496-web-<server_id>...')
+    (>&2 echo 'Example: sudo hostnamectl set-hostname 496-web-<insert_server_id_here>')
+fi
 
+# install nginx
+apt-get -y update
+apt-get -y install nginx
+
+# update 404 error page
+echo "Ceci n'est pas une page" > /usr/share/nginx/html/404.html
+
+# create static directories and links
+mkdir -p $STATIC/releases/test
+mkdir -p $STATIC/shared
+echo 'Holberton School Is Running!' > $STATIC/releases/test/index.html
+ln -sfn $STATIC/releases/test $STATIC/current
+sudo chown -f -R ubuntu:ubuntu /data/
+
+# update config file to redirect
 printf %s "server {
-    listen 80 default_server;
+    listen 80;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+    root   $STATIC/current;
+    index  index.html index.htm 8-index.html;
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
+    add_header X-Served-By $HOST_NAME;
+
+    location / {
+        alias $STATIC/current/;
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://google.com/;
+    }
+
+    location /hbnb_static {
+        alias $STATIC/current/;
     }
 
     error_page 404 /404.html;
     location /404 {
-      root /var/www/html;
+      root /usr/share/nginx/html;
       internal;
     }
-}" > /etc/nginx/sites-available/default
+}" > $CONFIG_FILE
+
+# start nginx after reloading config
+service nginx start
+# if nginx was already running restart it
 service nginx restart
